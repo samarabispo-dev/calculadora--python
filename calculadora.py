dd48e1c6474ff8cee675e43811338b4288708ea2
Samara Bispo <samara18bispo@gmail.com>
2026-03-13 11:50:01 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+print('calculadora Simples')
+num1 = foat(input('Digite o primeiro número: '))
+num2 = foat(input('Digite o segundo número: ' ))
+operacao = input('Escolha uma operação (+,-,*,/): ")
+if operacao == '+':
+print('Resultado:', num1 + num2)
+elif operacao == '-':
+print('Resultado:', num1 - num2)
+elif operacaco == '*':
+print('Resultado:', num1 * num2)
+elif operacao == '/':
+print('Resultado:', num1 /num2)
+else:
+print('Operação Inválida')
