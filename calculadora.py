7cef54480b1d464186469f39c3200defb21a8dcc
Samara Bispo <samara18bispo@gmail.com>
2026-09-22 10:52:03 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,27 @@
-print('calculadora Simples')
-num1 = foat(input('Digite o primeiro número: '))
-num2 = foat(input('Digite o segundo número: ' ))
-operacao = input('Escolha uma operação (+,-,*,/): ")
-if operacao == '+':
-print('Resultado:', num1 + num2)
-elif operacao == '-':
-print('Resultado:', num1 - num2)
-elif operacaco == '*':
-print('Resultado:', num1 * num2)
-elif operacao == '/':
-print('Resultado:', num1 /num2)
-else:
-print('Operação Inválida')
+def calcular(num1, num2, operacao):
+  if oeracao == "+":
+    return num1 + num2
+  elif operacao == "-":
+    return numm1 - num2
+  elif operacao == "*":
+    return num1 * num2
+  elif operacao == "/":
+    if num2 == 0:
+      return "Não é possível dividir por zero."
+    return num1 / num2
+  else:
+    return"Operação Inválida"
+print("====== CALCULADORA PYTHON =====")
+while True:
+  try:
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    operacao = input("Escolha uma operação(+,-,*,/):")
+    resultado = Calcular(num1, num2, operacao)
+      print(f"Resultado:{resultado}")
+  except ValueErro:
+    print("Digite apenas números válidos")
+    continuar = input("Deseja fazer outra operação? (s/n): ").lower()
+    if continuar != "S":
+      print("Calculadora Encerrada.")
+      break
